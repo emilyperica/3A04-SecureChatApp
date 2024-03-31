29e2f33d5d6772f14504dd6e1855a476c7c8a36e
Kd020819 <dengyq2002@outlook.com>
2024-03-31 17:21:50 -0400

    add new icons

--- Code/app/src/main/java/com/example/macmessenger/AccountManagement.java
+++ Code/app/src/main/java/com/example/macmessenger/AccountManagement.java
@@ -9,16 +9,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AccountManagement extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account_management);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+
 }
